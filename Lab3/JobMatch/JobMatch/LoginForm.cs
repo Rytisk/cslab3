9aa3ae49a868d3d5cc3445ab2234b29d04326276
Rytis Kaplūnas <rytkaplunas@gmail.com>
2016-12-05 16:11:24 +0200

    Added some forms

---
+++ Lab3/JobMatch/JobMatch/LoginForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JobMatch
+{
+    public enum Type
+    {
+        JobSeeker,
+        Employer
+    }
+    public partial class Form1 : Form
+    {
+        Type type;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (radioButton1.Checked)
+            {
+                type = Type.JobSeeker;
+            }
+            else if (radioButton2.Checked)
+            {
+                type = Type.Employer;
+            }
+            else
+            {
+                MessageBox.Show("Please choose a type of login");
+            }
+
+            
+            
+        }
+    }
+}
