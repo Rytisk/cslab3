a0457307bc7dc3e86054acab025e8b94e623f880
Rytis Kaplūnas <rytkaplunas@gmail.com>
2016-12-10 00:13:59 +0200

    Worked on SelectorForm

--- Lab3/JobMatch/JobMatch/DBHandler.cs
+++ Lab3/JobMatch/JobMatch/DBHandler.cs
@@ -9,11 +9,24 @@ namespace JobMatch
 {
     class DBHandler
     { 
-        public List<Employer> GetEmployers()
+        public List<DataForSelector> GetEmployer()
         {
             using (FindJobDBEntities db = new FindJobDBEntities())
             {
-                return db.Employer.ToList();
+                var list = (from emp in db.Employer
+                            join jb in db.Job on emp.Id equals jb.Employer_Id
+                            join skill in db.RequiredSkill on jb.RequiredSkill_Id equals skill.Id
+                            select new DataForSelector
+                            {
+                                CompanyName = emp.Name,
+                                Position = jb.Position,
+                                ShortJobDescription = jb.JobDescription,
+                                Education = jb.EducationRequirements,
+                                AdditionalRequirements = jb.AditionalRequirements
+
+
+                            }).ToList();
+                return list;
             }
         }
     }
---
+++ Lab3/JobMatch/JobMatch/DataForSelector.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+
+namespace JobMatch
+{
+    public class DataForSelector
+    {
+        public string AdditionalRequirements { get; set; }
+        public string CompanyName { get; set; }
+        public string Education { get; set; }
+        public string Position { get; set; }
+        public string ShortJobDescription { get; set; }
+        public IEnumerable Skills
+        {
+            get;
+            set;
+        }
+    }
+}
